4eb5d4bcca8cc49f30a9524685aebd0e3fb61fd7
plantmandev <plantmandev@gmail.com>
2026-04-25 14:23:03 -0600

    removed test file

--- test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from pathlib import Path
-
-df = pd.read_csv('occurrence-data/species-metadata.csv', dtype={'gbif_key': str})
-df['category'] = df['category'].replace('butterfly', 'lepidoptera')
-df.to_csv('occurrence-data/species-metadata.csv', index=False)
-print(df[['species_name', 'category', 'common_name']].to_string())
